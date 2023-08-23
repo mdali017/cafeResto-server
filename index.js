4bbf8ee921cd81eae9618f78331ff19c19aeeeee
MD MOHABBAT ALI <mohabbatalit8@gmail.com>
2023-08-23 09:34:16 +0600

    verifyJWT function create and Secure Cart Api

--- index.js
+++ index.js
@@ -8,6 +8,27 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: "unauthorized access"})
+  }
+
+  // bearer token
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+     if(err){
+      return res.status(401).send({error: true, message: "unauthorized access"})
+     }
+
+     req.decoded = decoded
+     next()
+
+  })
+
+}
+
 app.get('/', (req, res) => {
     res.send('Awesome Restuarant is running')
 })
@@ -35,6 +56,14 @@ async function run() {
     const cartCollections = client.db("cafe-Resto-DB").collection("carts")
     const userCollections = client.db("cafe-Resto-DB").collection("users")
 
+    // json web token related api
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+      res.send({token})
+    })
+
     // menu related api
     app.get('/menu', async(req, res) => {
       const result = await menuCollections.find().toArray();
@@ -48,19 +77,25 @@ async function run() {
     })
 
     // cart Related api
-    app.post('/carts', async(req, res) => {
+    app.post('/carts',  async(req, res) => {
       const item = req.body;
       // console.log(item)
       const result = await cartCollections.insertOne(item);
       res.send(result)
     })
 
-    app.get('/carts', async(req, res) => {
+    app.get('/carts', verifyJWT,  async(req, res) => {
       const email = req.query.email;
 
       if(!email){
         res.send([])
       }
+      
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail){
+        return res.status(403).send({error: true, message: "forbidden access"})
+      }
+
       const query = {email: email}
       const result = await cartCollections.find(query).toArray();
       res.send(result)
